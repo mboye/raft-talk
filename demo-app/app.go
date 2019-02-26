29cc8faa65e740bc362247132c3dda07a6a8d5a4
Magnus Boye <magnus.boye@zalando.fi>
2019-02-26 12:03:25 +0100

    Add demo app

---
+++ demo-app/app.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/mboye/raft-talk/demo-app/indexhandler"
+	log "github.com/sirupsen/logrus"
+	"go.etcd.io/etcd/clientv3"
+)
+
+func main() {
+
+	endpointsArg, found := os.LookupEnv("ETCD_ENDPOINTS")
+	if !found {
+		log.Fatal("environment variable missing: ETCD_ENDPOINTS")
+	}
+	endpoints := strings.Split(endpointsArg, ",")
+
+	cfg := clientv3.Config{
+		Endpoints: endpoints,
+	}
+
+	clusterClient, err := clientv3.New(cfg)
+	if err != nil {
+		log.WithField("err", err).Fatal("failed to create etcd client")
+	}
+
+	http.Handle("/", indexhandler.New(clusterClient))
+	log.Info("starting server on port 8080")
+	err = http.ListenAndServe("0.0.0.0:8080", nil)
+	log.WithError(err).Error("server stopped")
+}
---
+++ demo-app/indexhandler/index_handler.go
@@ -0,0 +1,95 @@
+package indexhandler
+
+import (
+	"context"
+	"html/template"
+	"net/http"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+	"go.etcd.io/etcd/clientv3"
+)
+
+const backgroundColorConfigKey = "app-config/background-color"
+const defaultBackgroundColor = "#ffffff"
+
+type indexHandler struct {
+	clusterClient   *clientv3.Client
+	indexTemplate   *template.Template
+	backgroundColor string
+	mutex           sync.RWMutex
+}
+
+// New returns a new request handler for the index page
+func New(clusterClient *clientv3.Client) http.Handler {
+	handler := &indexHandler{
+		clusterClient:   clusterClient,
+		backgroundColor: defaultBackgroundColor}
+
+	var err error
+	handler.indexTemplate, err = template.New("index").ParseFiles("templates/index.html")
+	if err != nil {
+		log.WithError(err).Fatal("failed to parse index template")
+	}
+
+	handler.loadBackgroundColor()
+	go handler.watchBackgroundColor()
+	return handler
+}
+
+func (h *indexHandler) loadBackgroundColor() {
+	backgroundColor := defaultBackgroundColor
+	response, err := h.clusterClient.Get(context.Background(), backgroundColorConfigKey)
+	if err != nil {
+		log.WithError(err).Error("failed to get background color from etcd")
+	} else {
+		found := false
+		for _, kv := range response.Kvs {
+			if string(kv.Key) == backgroundColorConfigKey {
+				found = true
+				backgroundColor = string(kv.Value)
+				log.WithField("background_color", backgroundColor).Info("loaded background color from etcd")
+				break
+			}
+		}
+
+		if !found {
+			log.Error("background color configuration not found in etcd")
+		}
+	}
+
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	h.backgroundColor = backgroundColor
+	log.WithField("background_color", backgroundColor).Info("current background color")
+}
+
+func (h *indexHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	h.mutex.RLock()
+	defer h.mutex.RUnlock()
+	h.indexTemplate.ExecuteTemplate(response, "index.html", h.backgroundColor)
+	log.WithFields(log.Fields{
+		"path":       request.URL.Path,
+		"method":     request.Method,
+		"user_agent": request.Header.Get("user-agent")}).Info("handled request")
+}
+
+func (h *indexHandler) watchBackgroundColor() {
+	log.Info("watching for background color changes in etcd")
+	changes := h.clusterClient.Watch(context.Background(), backgroundColorConfigKey, clientv3.WithPrevKV())
+	for change := range changes {
+		for _, event := range change.Events {
+			if string(event.Kv.Key) == backgroundColorConfigKey {
+				oldValue := string(event.PrevKv.Value)
+				newValue := string(event.Kv.Value)
+				log.WithFields(log.Fields{
+					"old_value": oldValue,
+					"new_value": newValue}).Info("background color changed")
+
+				h.mutex.Lock()
+				defer h.mutex.Unlock()
+				h.backgroundColor = newValue
+			}
+		}
+	}
+}
